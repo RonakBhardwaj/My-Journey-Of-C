28818ff5623a09063674ff28aa30fa96dd0e9035
RonakBhardwaj <ronakbh949471@gmail.com>
2024-11-16 23:10:28 +0530

    add new content

---
+++ Basics of C/constants.c
@@ -0,0 +1,19 @@
+/*
+A constant is an entity that doesn't change in programming language constants are often called literals.
+Types of Constants:
+Constants are divided into two major categories:-
+1.Primary Constant 
+2.Secondary Constant
+
+1.Primary Consatnt 
+-> Integer Constant 
+-> Real Constant 
+-> Character Constant 
+
+2.Secondary Constant
+->Array
+->Pointer
+->Structure
+->Union
+->Enum etc.
+*/
